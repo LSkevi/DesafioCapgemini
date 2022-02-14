5b394100954a91bc4cac53e0a906b3299be5630f
LSkevi <99023092+LSkevi@users.noreply.github.com>
2022-02-14 17:11:10 -0300

    Update Escada.java

--- Escada01/src/Escada.java
+++ Escada01/src/Escada.java
@@ -5,26 +5,23 @@ public class Escada {
 	
 	public static void main (String []args) {
 		
-	try (Scanner ler = new Scanner(System.in)) {
+	
+	try(Scanner ler = new Scanner(System.in)) {
 		System.out.print("Insira o valor de n: ");
 		
-		int n = ler.nextInt();
-			for(int repetir = 0;repetir < n;repetir++) {
-				for(int esp = 1; esp <n - repetir;esp++) {
-					System.out.print(" ");
-				}
-				 for (int ast = 0; ast < repetir + 1; ast++) {
-		                System.out.print("*");
-		            }
-				System.out.println();
-			}
-	}
-		}
+	int n = ler.nextInt();
 	
+	for(int repetir = 0;repetir < n;repetir++) {
+		for(int esp = 1; esp <n - repetir;esp++) {
+			System.out.print(" "); }
 		
+		for (int ast = 0; ast < repetir + 1; ast++){
+			System.out.print("*");}
 		
-				
+	System.out.println();}
+		}
 	}
+}
 	
 	
 
